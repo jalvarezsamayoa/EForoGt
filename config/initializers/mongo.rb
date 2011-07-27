ece3ca769ab00c262a82b29a7ec592dca00b4f81
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-07-26 20:42:23 -0600

    actualizado conf de mongo

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,14 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#eforo-#{Rails.env}"
+if Rails.env.production?
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10075, { :logger => Rails.logger })
+  MongoMapper.database = 'app646939'
+  MongoMapper.database.authenticate('jalvarezsamayoa', 'Bless777')
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "#eforo-#{Rails.env}"
+end
 
 if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect if forked
-   end
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    MongoMapper.connection.connect if forked
+  end
 end
